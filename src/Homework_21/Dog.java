7eaf3642a19f60af6c6352f899f970d0433b6919
VitaliiPid <vitalii.piddubnyi@gmail.com>
2023-12-08 09:02:07 +0100

    Homework 21

--- src/Homework_21/Dog.java
+++ src/Homework_21/Dog.java
@@ -2,20 +2,17 @@ package Homework_21;
 
 public class Dog {
     private String name;
-    private static int counter = 0; // Статическое поле для отслеживания количества созданных объектов
+    private static int counter = 0;
 
-    // Конструктор класса
     public Dog(String name) {
         this.name = name;
-        counter++; // Увеличиваем счетчик при создании нового объекта
+        counter++;
     }
 
-    // Геттер для получения значения счетчика
     public static int getCounter() {
         return counter;
     }
 
-    // Геттер для получения имени собаки
     public String getName() {
         return name;
     }
