1e4e5fd096bcfd3de691bfeacad8db8717185844
VitaliiPid <vitalii.piddubnyi@gmail.com>
2023-12-13 09:04:34 +0100

    Homework 24 Inheritance

--- src/Homework_24/Car.java
+++ src/Homework_24/Car.java
@@ -7,4 +7,15 @@ public class Car extends Transport{
         super(brand, model);
         this.numberOfDoors = numberOfDoors;
     }
+
+    public void start() {
+        super.start();
+        System.out.println("Number of doors: " + numberOfDoors);
+    }
+
+    public void stop() {
+        super.stop();
+        System.out.println("Number of doors: " + numberOfDoors);
+
+    }
 }
--- src/Homework_24/Main.java
+++ src/Homework_24/Main.java
@@ -6,15 +6,18 @@ public class Main {
     Task 1
     Написать как минимум одну иерархию классов в которой будет один родительский класс и два наследника,
     расширяющих его функционал
+
+    Разработать классы для представления транспортных средств. Базовый класс Transport должен содержать
+    информацию о марке и модели, а также методы start() и stop(). Реализовать подклассы: Car с информацией
+    о количестве дверей и Moto с информацией о типе двигателя.
      */
+        public static void main(String[] args) {
+            Car car = new Car("Toyota", "Camry", 4);
+            car.start();
+            car.stop();
 
-    public static void main(String[] args) {
-        Car car = new Car("Toyota", "Camry", 4);
-        car.start();
-        car.stop();
-
-        Moto motorcycle = new Moto("Harley-Davidson", "Sportster", "Gasoline");
-        motorcycle.start();
-        motorcycle.stop();
-    }
+            Moto moto = new Moto("Harley-Davidson", "Sportster", "Gasoline");
+            moto.start();
+            moto.stop();
+        }
 }
